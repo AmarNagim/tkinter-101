8d321676e081dc6c44c4aca89b9d147ef59f9282
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-11-17 10:18:52 +0100

    explosief-venster

---
+++ gui.py
@@ -0,0 +1,52 @@
+import tkinter
+import time
+
+window = tkinter.Tk()
+
+window.title('My First Window')
+window.geometry('50x50')
+print('Background 1')
+
+def background2():
+    window.configure(bg='red')
+    window.geometry('100x100')
+    print('Background 2')
+    
+ 
+def background3():
+    window.configure(bg='blue')
+    window.geometry('150x150')    
+    print('Background 3')
+
+def background4():
+    window.configure(bg='orange')
+    window.geometry('200x200')     
+    print('Background 4')
+
+def background5():
+    window.configure(bg='yellow')
+    window.geometry('250x250')
+    print('Background 5')
+
+def background6():
+    window.configure(bg='green')
+    window.geometry('300x300')    
+    print('Background 6')
+
+def destroy():
+    window.destroy()
+    print('BOOM!')
+
+    
+window.after(2000, background2)
+window.after(4000, background3)
+window.after(6000, background4)
+window.after(8000, background5)
+window.after(10000, background6)
+window.after(12000, lambda:destroy())
+
+
+
+
+
+window.mainloop()
