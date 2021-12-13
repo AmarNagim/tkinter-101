aa49b16e0943a8471195f25b2479707048204c1f
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-12-13 13:50:32 +0100

    latest changes

--- gui.py
+++ gui.py
@@ -8,28 +8,28 @@ window.geometry('50x50')
 print('Background 1')
 
 def background2():
-    window.configure(bg='red')
+    window.configure(bg='pink')
     window.geometry('100x100')
     print('Background 2')
     
  
 def background3():
-    window.configure(bg='blue')
+    window.configure(bg='orange')
     window.geometry('150x150')    
     print('Background 3')
 
 def background4():
-    window.configure(bg='orange')
+    window.configure(bg='blue')
     window.geometry('200x200')     
     print('Background 4')
 
 def background5():
-    window.configure(bg='yellow')
+    window.configure(bg='red')
     window.geometry('250x250')
     print('Background 5')
 
 def background6():
-    window.configure(bg='green')
+    window.configure(bg='purple')
     window.geometry('300x300')    
     print('Background 6')
 
